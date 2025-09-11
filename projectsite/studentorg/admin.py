78658a9a9acdcd6317211ca676a1bce7dec75171
Agonnie2326uwu <carldecastro989@gmail.com>
2025-09-11 23:53:01 +0800

    finfr

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -6,23 +6,19 @@ from .models import College, Program, Organization, Student, OrgMember
 
 @admin.register(Student) 
 class StudentAdmin(admin.ModelAdmin): 
-    list_display = ("student_id", "lastname", 
-                    "firstname", "middlename", "program") 
-    search_fields = ("lastname", "firstname",) 
+    list_display = ("student_id", "lastname", "firstname", "middlename", "program") 
+    search_fields = ("lastname", "firstname", "student_id") 
 
 
 @admin.register(OrgMember) 
 class OrgMemberAdmin(admin.ModelAdmin):  
-    list_display = ("student", "get_member_program", "organization",  
-                    "date_joined",)  
+    list_display = ("student", "get_member_program", "organization",   "date_joined",)  
     search_fields = ("student__lastname", "student__firstname",)  
   
     def get_member_program(self, obj): 
-        try: 
-            member = Student.objects.get(id=obj.student_id) 
-            return member.program 
-        except Student.DoesNotExist: 
-            return None 
+        return obj.student.program if obj.student else None
+    
+    get_member_program.short_description = 'Program'
 
 @admin.register(College)
 class CollegeAdmin(admin.ModelAdmin):
